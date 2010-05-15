f905cfe3896fd48f24762f4b1f31d2353f1e4449
Eric Kidd <git@randomhacks.net>
2010-05-15 19:26:53 -0400

    Copy spec structure from mongoid_references gem and tweak

---
+++ spec/mongoid_attr_accessible_spec.rb
@@ -0,0 +1,7 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe Mongoid, ".attr_accessible" do
+  it "should exist" do
+    
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.setup(:default, :development)
+
+require 'spec'
+require 'spec/autorun'
+require 'mocha'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(File.dirname(__FILE__)), 'lib'))
+
+require 'mongoid_attr_accessible'
+
+def fixture_path(path)
+  File.join(File.dirname(__FILE__), 'fixtures', path)
+end
+
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db('mongoid_attr_accessible_test')
+end
+
+Spec::Runner.configure do |config|  
+end
