eb9eac5c456e88033389eb75b0ec4e3afeebc1a3
Eric Kidd <git@randomhacks.net>
2010-05-16 08:21:36 -0400

    Implement basic attr_accessible
    This version is still incomplete.  See the spec file for a list of
    other behaviors we need to verify.

--- lib/mongoid_attr_accessible.rb
+++ lib/mongoid_attr_accessible.rb
@@ -1,2 +1,50 @@
 require 'mongoid'
 
+module Mongoid
+  module Fields
+    module ClassMethods
+      def field_with_attr_accessible(name, options = {})
+        if options[:accessible]
+          attr_accessible(name)
+        end
+        field_without_attr_accessible(name, options)
+      end
+      alias_method :field_without_attr_accessible, :field
+      alias_method :field, :field_with_attr_accessible
+    end
+  end
+
+  module Attributes
+    module ClassMethods
+      # Mark attrs as accessible, and change the default for all other
+      # attributes to non-accessible.
+      def attr_accessible *attrs
+        @attr_accessible ||= []
+        @attr_accessible.concat(attrs.map {|a| a.to_sym })
+      end
+
+      # Does this class have attr_accessible?
+      def have_attr_accessible?
+        !@attr_accessible.nil?
+      end
+
+      # Is attr accessible via bulk update?
+      def attr_accessible?(attr)
+        @attr_accessible.include?(attr.to_sym)
+      end
+    end
+
+    module InstanceMethods
+      # Can we write to attr?
+      def write_allowed_with_attr_accessible?(attr)
+        if self.class.have_attr_accessible?
+          self.class.attr_accessible?(attr)
+        else
+          write_allowed_without_attr_accessible?(attr)
+        end
+      end
+      alias_method :write_allowed_without_attr_accessible?, :write_allowed?
+      alias_method :write_allowed?, :write_allowed_with_attr_accessible?
+    end
+  end
+end
--- spec/mongoid_attr_accessible_spec.rb
+++ spec/mongoid_attr_accessible_spec.rb
@@ -1,7 +1,45 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
+class User
+  include Mongoid::Document
+
+  field :name
+  field :email, :accessible => true
+  field :security_key
+
+  attr_accessible :name
+end
+
 describe Mongoid, ".attr_accessible" do
-  it "should exist" do
-    
+  context "ordinary fields" do
+    it "should be bulk updatable if named" do
+      User.new(:name => 'Foo').name.should == 'Foo'
+    end
+
+    it "should be bulk updatable if declared with :accessible => true" do
+      User.new(:email => 'foo@example.com').email.should == 'foo@example.com'
+    end
+
+    it "should not be bulk updatable otherwise" do
+      user = User.new(:security_key => 'evil value')
+      user.security_key.should be_nil
+    end
+
+    it "should always be settable using direct assignment" do
+      user = User.new
+      user.security_key = 'directly-assigned value'
+      user.security_key.should == 'directly-assigned value'
+    end
+
+    it "should be settable using a block" do
+      user = User.new {|u| u.security_key = 'block-assigned value' }
+      user.security_key.should == 'block-assigned value'
+    end
   end
+
+  # it "should allow either strings or symbols"
+  # it "should combine multiple declarations"
+  # context "other setters"
+  # context "associations"
+  # context "classes without attr_accessible"
 end
